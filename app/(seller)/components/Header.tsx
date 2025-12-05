a7b9b1519676c56bdff3233a48aba22e108d26b0
biormarcus <biormarcus@gmail.com>
2025-12-05 09:46:28 +0300

    WIP

--- app/(seller)/components/Header.tsx
+++ app/(seller)/components/Header.tsx
@@ -81,7 +81,7 @@ export function Header({ toggleSidebar }: { toggleSidebar: () => void }) {
                 <MenuItem>
                   {({ active }) => (
                     <Link
-                      href="/accounts-settings/profile"
+                      href="/account-settings"
                       className={`flex items-center px-4 py-2 text-sm ${
                         active ? "bg-gray-100 text-gray-900" : "text-gray-700"
                       }`}
